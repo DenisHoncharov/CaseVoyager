ad060601b1c93866bd1ab1673aa702c506db6ec8
d.goncharov <d.goncharov@aimprosoft.com>
2023-12-04 20:03:23 +0200

    Initial commit
    Set up the standard laravel project with Spatie role permissions library

---
+++ README.md
@@ -0,0 +1,11 @@
+## Installation guid
+
+- composer install
+- php artisan key:generate
+- cp .env.example .env
+- Add your database credentials to the ".env" file
+- php migrate
+- php db:seed
+- php artisan serve //now you can get access to the site using http://127.0.0.1:8000
+- In browser go to http://127.0.0.1:8000/admin
+- By default, you can use "admin@admin.com" as login and "password" as password
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Auto generated seed file.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (User::count() == 0) {
+            $role = Role::create(['name' => 'Admin']);
+            $permission = Permission::create(['name' => 'Add Cases']);
+
+            $role->givePermissionTo($permission);
+            $permission->assignRole($role);
+
+            $user = User::create([
+                'name'           => 'Admin',
+                'email'          => 'admin@admin.com',
+                'password'       => bcrypt('password'),
+                'remember_token' => Str::random(60)
+            ]);
+
+            $user->assignRole('Admin');
+        }
+    }
+}
